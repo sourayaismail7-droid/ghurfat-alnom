d06caf8fcfaa5e6230e01a0b570fe4c952b965ca
sourayaismail7-droid <souraya.ismail7@gmail.com>
2026-08-27 18:50:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -297,7 +297,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+   const PORT = process.env.PORT || 3000;
+   server.listen(PORT, () => {
   console.log('🌙 Server running at http://localhost:3000');
   console.log('🔑 Default Admin PIN: 9999');
 });
